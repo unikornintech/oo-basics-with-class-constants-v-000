342d548c3549d54987d723d78bcff565b75f8033
Lauren Days <lmchambers0420@gmail.com>
2019-01-13 22:25:41 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -11,5 +11,10 @@ class Book
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
+  # creaye the writer for genre and add the logic for the class constant 
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+  end 
 
 end
