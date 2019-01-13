1e6b81d21b0a8aa3af6ca8880450722e82653d6e
Lauren Days <lmchambers0420@gmail.com>
2019-01-13 22:34:28 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,10 @@
+# book.rb 
+
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count # remove the attr_accessor for genre :genre
+  attr_reader :title, :genre # add an attr_reader for genre
+  
+  GENRES = [ ]
 
   def initialize(title)
     @title = title
@@ -9,5 +13,10 @@ class Book
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
+  # create the writer for genre and add the logic for the class constant 
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+  end 
 
 end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,14 @@
+# shoe.rb 
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  
+  BRANDS = [ ]
 
   def initialize(brand)
     @brand = brand
+    BRANDS << brand unless BRANDS.include?(brand)
   end
 
   def cobble
