88c97d6ec95b4203e7e87a08e0ef3adecc53eb07
Lauren Days <lmchambers0420@gmail.com>
2019-01-13 22:27:33 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,7 @@
 # book.rb 
 
 class Book
-  attr_accessor :author, :page_count, # remove the attr_accessor for genre :genre
+  attr_accessor :author, :page_count # remove the attr_accessor for genre :genre
   attr_reader :title, :genre # add an attr_reader for genre
   
   GENRES = [ ]
